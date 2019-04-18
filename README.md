9242193e2f93d37ef4a57b64db649884d05975e7
Bianca <biancacristina-silva@outlook.com>
2019-04-18 09:57:51 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Homologation-System-backend
---
+++ src/main/kotlin/com/github/biancacristina/HomologationSystem/HomologationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.github.biancacristina.HomologationSystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomologationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomologationSystemApplication>(*args)
+}
